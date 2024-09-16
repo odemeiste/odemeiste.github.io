17faa0b752826968e74bb68796604dccdde96048
Selin Yorulmaz <seyorulmaz@bkm.local>
2024-09-16 14:25:40 +0300

    Aktif sürüm paylaşılmıştır.

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,8 +1,52 @@
 module.exports = [
 
-
     {
-        title: 'SÜRÜM 1.0.0-Aktif Sürüm',
+        title: 'SÜRÜM 2.0.0-Aktif Sürüm',
+        path: '/v2.0.0/contents/tanim-ve-kisitlamalar',
+        children: [
+            {
+                title: '1. Tanımlar ve Kısıtlamalar',
+                path: '/v2.0.0/contents/tanim-ve-kisitlamalar'
+            },
+            {
+                title: '2. Amaç ve Kapsam',
+                path: '/v2.0.0/contents/amac-ve-kapsam'
+            },
+            {
+                title: '3. Temel Prensipler',
+                path: '/v2.0.0/contents/temel-prensipler'
+            },
+            {
+                title: '4. Ödeme İste Durumları',
+                path: '/v2.0.0/contents/odeme-iste-durumlari'
+            },
+            {
+                title: '5. Ödeme İste Sistemi ve Kavramlar',
+                path: '/v2.0.0/contents/odeme-iste-sistemi-ve-kavramlar'
+            },
+            {
+                title: '6. Ödeme İste Kullanım Modelleri ve Fonksiyonlar',
+                path: '/v2.0.0/contents/odeme-iste-modelleri'
+            },
+
+            {
+                title: '7. Ödeme İste',
+                path: '/v2.0.0/contents/odeme-iste'
+            },
+
+            {
+                title: '8. Ödeme İste Zaman Aşımı ve FAST Doğrulama Kontrolleri',
+                path: '/v2.0.0/contents/katilimci-dogrulama'
+            },
+            {
+                title: '9. OHS API',
+                path: '/v2.0.0/contents/ohs-api'
+            },
+           
+        ]
+    },
+    {
+        title: 'SÜRÜM 1.0.0',
         path: '/v1.0.0/contents/tanim-ve-kisitlamalar',
         children: [
             {
@@ -48,53 +92,14 @@ module.exports = [
         ]
     },
 
+
     {
-        title: 'SÜRÜM 2.0.0-Taslak Sürüm',
-        path: '/v2.0.0/contents/tanim-ve-kisitlamalar',
-        children: [
-            {
-                title: '1. Tanımlar ve Kısıtlamalar',
-                path: '/v2.0.0/contents/tanim-ve-kisitlamalar'
-            },
-            {
-                title: '2. Amaç ve Kapsam',
-                path: '/v2.0.0/contents/amac-ve-kapsam'
-            },
-            {
-                title: '3. Temel Prensipler',
-                path: '/v2.0.0/contents/temel-prensipler'
-            },
-            {
-                title: '4. Ödeme İste Durumları',
-                path: '/v2.0.0/contents/odeme-iste-durumlari'
-            },
-            {
-                title: '5. Ödeme İste Sistemi ve Kavramlar',
-                path: '/v2.0.0/contents/odeme-iste-sistemi-ve-kavramlar'
-            },
-            {
-                title: '6. Ödeme İste Kullanım Modelleri ve Fonksiyonlar',
-                path: '/v2.0.0/contents/odeme-iste-modelleri'
-            },
+        title: 'Sıkça Sorulan Sorular',
+        path: '/sorulan-sorular',
 
-            {
-                title: '7. Ödeme İste',
-                path: '/v2.0.0/contents/odeme-iste'
-            },
-
-            {
-                title: '8. Ödeme İste Zaman Aşımı ve FAST Doğrulama Kontrolleri',
-                path: '/v2.0.0/contents/katilimci-dogrulama'
-            },
-            {
-                title: '9. OHS API',
-                path: '/v2.0.0/contents/ohs-api'
-            },
-           
-        ]
     },
     {
-        title: 'Öİ Kurumsal API-Taslak Sürüm',
+        title: 'Öİ Kurumsal API 1.0.0-Aktif Sürüm',
         path: '/kurumsal-api/contents/tanim-ve-kisitlamalar',
         children: [
             {
@@ -130,10 +135,4 @@ module.exports = [
            
         ]
     },
-    {
-        title: 'Sıkça Sorulan Sorular',
-        path: '/sorulan-sorular',
-
-    },
-
 ];
